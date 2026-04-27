d3ba98ad7b04046cf21f78c203bf373ef61cd302
gurusso <gumoocaa@gmail.com>
2026-04-27 10:39:44 -0300

    Inicio exercicío interface CMD

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("==============================\n");
+    printf("SISTEMA EQUIPE XYZ\n");
+    printf("==============================\n");
+    printf("1 - Inserir notas\n");
+    printf("2 - Calcular media\n");
+    printf("3 - Verificar situacao\n");
+    printf("4 - Exibir resultado\n");
+    printf("5 - Calcular derivada\n");
+    printf("6 - Sair\n");
+    printf("Escolha uma opcao: ");
+    
+    
+    return 0;   
+}
