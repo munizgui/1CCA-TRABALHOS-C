617f7cc3d789359e853c98cc8f1621ae1a801d5b
guilhermemuniz <guilhermemuniz181981@gmail.com>
2026-04-27 11:06:18 -0300

    adicionando while

--- main.c
+++ main.c
@@ -1,17 +1,38 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-int main() {
+void menu() {
     printf("==============================\n");
-    printf("SISTEMA EQUIPE XYZ\n");
+    printf("SISTEMA EQUIPE 1CCA\n");
     printf("==============================\n");
     printf("1 - Inserir notas\n");
     printf("2 - Calcular media\n");
     printf("3 - Verificar situacao\n");
     printf("4 - Exibir resultado\n");
-    printf("5 - Calcular derivada\n");
-    printf("6 - Sair\n");
+    printf("5 - Sair\n");
     printf("Escolha uma opcao: ");
-    
-    
-    return 0;   
 }
+
+int main() {
+    int opcao;
+    float nota_1 = 0, nota_2 = 0, media = 0;
+
+    while (true) {
+        menu();
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Insira a primeira nota: ");
+                scanf("%f", &nota_1);
+                printf("Insira a segunda nota: ");
+                scanf("%f", &nota_2);
+                break;
+        }
+    }
+
+    return 0;
+
+}
+
+        
